70679a1231bfe65cb1271cb2bd2742b8af1857ca
kiji-haru <kijiharu3112@outlook.jp>
2023-04-01 19:57:39 +0900

    feat:NotFoundのページをなんかそれっぽくした(いいえ)

--- src/pages/notfound.tsx
+++ src/pages/notfound.tsx
@@ -1,8 +1,25 @@
-import { Text } from "@chakra-ui/react";
+import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
 export const NotFound = () => {
   return (
-    <>
-      <Text>Not Found</Text>
-    </>
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+      backgroundColor="gray.50"
+    >
+      <Flex direction="column" alignItems="center">
+        <Heading as="h1" size="xl" mb="8">
+          Not Found
+        </Heading>
+        <Text fontSize="xl" mb="4">
+          The page you are looking for does not exist.
+        </Text>
+        <Text fontSize="xl" mb="8">
+          Please check the URL or go to the <Link to="/">home page</Link>.
+        </Text>
+      </Flex>
+    </Flex>
   );
 };
